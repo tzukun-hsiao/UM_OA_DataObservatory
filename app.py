44d4f6a6b556d30af14a0bba5bca4ef3751be466
TK-Hsiao <tkhsiao91@gmail.com>
2026-06-09 00:46:10 -0500

    update pages

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ page_1 = st.Page("pages/1_overall.py", title="1. Temporal Trends - Publications"
 #page_2 = st.Page("pages/2_knowledge_flow.py", title="2. Visibility", icon=None)
 
 # Set up navigation
-pg = st.navigation([main_page, page_1, page_2])
+pg = st.navigation([main_page, page_1])
 
 # Run the selected page
 pg.run()
