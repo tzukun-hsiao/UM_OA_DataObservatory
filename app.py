7afb11e089b0ff8f10468eb76b25bc8be3eadd94
TK-Hsiao <tkhsiao91@gmail.com>
2026-06-09 00:46:38 -0500

    update pages

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 main_page = st.Page("pages/0_welcome.py", title="Main Page", icon=None)
 page_1 = st.Page("pages/1_overall.py", title="1. Temporal Trends - Publications", icon=None)
-#page_2 = st.Page("pages/2_knowledge_flow.py", title="2. Visibility", icon=None)
+page_2 = st.Page("pages/2_knowledge_flow.py", title="2. Visibility", icon=None)
 
 # Set up navigation
-pg = st.navigation([main_page, page_1])
+pg = st.navigation([main_page, page_1, page_2])
 
 # Run the selected page
 pg.run()
